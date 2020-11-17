55525ae25f409546ffa9d56766a95f8a59c0d9a3
pritckat <pritckat@gmail.com>
2020-11-17 16:03:00 -0500

    clear button creates new n dimension grid

--- etch.js
+++ etch.js
@@ -7,12 +7,20 @@ function createDivs() {
 function clearGrid() {
     let container = document.getElementById('grid')
     container.innerHTML = '';
+    makeNewGrid();
 }
-for (i=0; i < 16; i++) {
-    for (j=0; j<16; j++) {
-        createDivs();
+
+function makeNewGrid() {
+    let numberOfRows = prompt("How many rows would you like?")
+    for (i=0; i < numberOfRows; i++) {
+        for (j=0; j<numberOfRows; j++) {
+            createDivs();
+        }
     }
+    let grid = document.getElementById('grid')
+    grid.style.gridTemplateColumns = (`repeat(${numberOfRows}, 0fr`)
 }
+
 
 let grid = document.getElementById('grid')
 grid.addEventListener("mouseover", function(event) {
@@ -21,3 +29,9 @@ grid.addEventListener("mouseover", function(event) {
         event.target.style.background = "";
     }, 500);
 }, false);
+
+for (i=0; i < 16; i++) {
+    for (j=0; j<16; j++) {
+        createDivs();
+    }
+}
